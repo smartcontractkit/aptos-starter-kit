4b74eb30821cc8b9cadf1a5b3561d3dd036fab6f
SyedAsadKazmi <syed.asadkazmi@smartcontract.com>
2025-06-12 18:46:01 +0530

    Added scripts for token transfer from Sepolia to Aptos

---
+++ scripts/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+    aptos: {
+        chainSelector: "743186221051783445",
+        ccipObjectAddress: "0xe9dbfbe04b92f7df3b6ab946ec62f88b37c638f25122ec0d0a99a5fd64ac0dd3",
+        ccipRouterModuleName: "router",
+        ccipOfframpModuleName: "offramp",
+        ccipOnrampModuleName: "onramp",
+        ccipBnMTokenAddress: "0x278e411ae68131d9e87213761f89db960c51f8b2e93cabfa07f034a436a883a3",
+        linkTokenAddress: "0x8c20817245024d8cdfeb7b1e673e8e470585e74f7b860962c3caa668c2fa3542",
+    },
+    sepolia: {
+        chainSelector: "16015286601757825753",
+        ccipRouterAddress: "0x85634Ebafbc5D71d6606D4ea76630941B0e18Cee",
+        ccipOfframpAddress: "0xe3d660848B680355a90b8E7fD4E4a1f63F3522D7",
+        ccipOnrampAddress: "0x48B2e5D487Cb85a7586FCdbDF9cC4E8c7391ED1B",
+        ccipBnMTokenAddress: "0xFd57b4ddBf88a4e07fF4e34C487b99af2Fe82a05",
+        linkTokenAddress: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
+    }
+}
